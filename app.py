392c10a6cabd1bec0b38e8b87cf156c5056add9d
Zaila Analytics <analytics.zaila@gmail.com>
2025-02-15 15:12:16 +0000

    species name change

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    return render_template("index.html", prediction_text = "The flower belongs to species {}".format(prediction))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
